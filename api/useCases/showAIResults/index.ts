1e04d3061214a4b129a92fb145a85a9edf4f296c
RSobrinho <rafarrsobrinho@hotmail.com>
2023-05-18 14:41:39 -0400

    fix + deploy: folders backend frontend separation + deploy configs

--- api/useCases/showAIResults/index.ts
+++
@@ -1,8 +0,0 @@
-// import { GetSentimentUseCase } from './getSentimentUseCase'
-// import { GetSentimentController } from './getSentimentController'
-// import { robertaTrainedAIProvider } from '../../providers/implementations/robertaTrainedAIProvider'
-
-// const getSentimentUseCase = new GetSentimentUseCase(robertaTrainedAIProvider)
-// const getSentimentController = new GetSentimentController(getSentimentUseCase)
-
-// export { getSentimentUseCase, getSentimentController }
