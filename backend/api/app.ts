63376c1aacc32a5227a1cb9b0c011a07375e286a
RSobrinho <rafarrsobrinho@hotmail.com>
2023-05-19 12:02:25 -0400

    feat(backend): otimizing folders and files

--- backend/api/app.ts
+++ backend/api/app.ts
@@ -1,15 +1,9 @@
 import express from "express";
-import swaggerUi from "swagger-ui-express";
-import swaggerDocs from "./swagger.json";
-import BaseRouter from "./routes/baseRouter";
 import ScrapingRouter from "./routes/scrapingRouter";
 import NewsRouter from "./routes/newsRouter";
-import UserRouter from "./routes/userRouter";
 import mongoose from "mongoose";
 import { errorResponse } from "./errors/handler";
 import { config } from "dotenv";
-import { join } from "path";
-import cors from "cors";
 
 class App {
   public apiRoute = "/api/v1";
@@ -21,25 +15,13 @@ class App {
     this.express = express();
     this.middlewares();
     this.database();
-    this.viewsSettings();
     this.routes();
     this.errorMiddlewares();
   }
 
-  private viewsSettings() {
-    this.express.set("view engine", "pug");
-    this.express.set("views", join(__dirname, "views"));
-  }
 
   private middlewares() {
     this.express.use(express.json());
-    // this.express.use(express.static(join(__dirname, "views")));
-    // this.express.use(cors());
-    this.express.use(
-      "/api/v1/api-docs",
-      swaggerUi.serve,
-      swaggerUi.setup(swaggerDocs)
-    );
   }
 
   private database() {
@@ -52,15 +34,12 @@ class App {
   }
 
   private errorMiddlewares() {
-    // this.express.use(errorLogging)
     this.express.use(errorResponse);
   }
 
   private routes() {
-    // this.express.use('/', BaseRouter);
     this.express.use(`${this.apiRoute}/news`, NewsRouter);
     this.express.use(`${this.apiRoute}/scraping`, ScrapingRouter);
-    this.express.use(`${this.apiRoute}/users`, UserRouter);
   }
 }
 
--- backend/api/useCases/user/get/index.ts
+++
@@ -1,9 +0,0 @@
-import { MongoDBUserRepository } from '../../../repositories/implementations/MongoDB/mongoDBUserRepository'
-import { GetUseCase } from './getUseCase'
-import { GetController } from './getController'
-
-const mongoDBUserRepository = new MongoDBUserRepository()
-const getUseCase = new GetUseCase(mongoDBUserRepository)
-const getController = new GetController(getUseCase)
-
-export { getUseCase, getController }
