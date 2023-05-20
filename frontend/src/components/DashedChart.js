e597016acb1e9b24d816d7147a70b85771f56db5
EduardoSLuz <edyven100@gmail.com>
2023-05-20 01:39:34 -0400

    fix: removing grafic

--- frontend/src/components/DashedChart.js
+++ frontend/src/components/DashedChart.js
@@ -6,52 +6,67 @@ export default function DashedChart() {
     series: [
       {
         name: "Negativo",
-        data: [45, 52, 38, 24, 33, 26, 21, 20, 6, 8, 15, 10]
+        data: [45, 52, 38, 24, 33, 26, 21, 20, 6, 8, 15, 10],
       },
       {
         name: "Neutro",
-        data: [35, 41, 62, 42, 13, 18, 29, 37, 36, 51, 32, 35]
+        data: [35, 41, 62, 42, 13, 18, 29, 37, 36, 51, 32, 35],
       },
       {
-            name: 'Positivo',
-        data: [87, 57, 74, 99, 75, 38, 62, 47, 82, 56, 45, 47]
-      }
+        name: "Positivo",
+        data: [87, 57, 74, 99, 75, 38, 62, 47, 82, 56, 45, 47],
+      },
     ],
     options: {
       chart: {
         type: "line",
         zoom: {
-            enabled: false
-          },
+          enabled: false,
+        },
       },
       dataLabels: {
-        enabled: false
+        enabled: false,
       },
       stroke: {
         width: [5, 5, 5],
-        curve: 'straight',
+        curve: "straight",
         dashArray: [7, 4, 0],
-        colors: ['#D21312', '#FFD93D', '#54B435'] // QUANDO EU TENTO ALTERAR A COR A COR DA LEGENDA NÃO MUDA
+        colors: ["#D21312", "#FFD93D", "#54B435"], // QUANDO EU TENTO ALTERAR A COR A COR DA LEGENDA NÃO MUDA
       },
       title: {
-        text: 'Gráfico de Linhas',
-        align: 'left'
+        text: "Gráfico de Linhas",
+        align: "left",
       },
       legend: {
-        tooltipHoverFormatter: function(val, opts) {
-          return val + ' - ' + opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] + ''
-        }
-        
+        tooltipHoverFormatter: function (val, opts) {
+          return (
+            val +
+            " - " +
+            opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] +
+            ""
+          );
+        },
       },
       markers: {
         size: 0,
         hover: {
-          sizeOffset: 6
-        }
+          sizeOffset: 6,
+        },
       },
       xaxis: {
-        categories: ['01 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
-          '10 Jan', '11 Jan', '12 Jan'
+        categories: [
+          "01 Jan",
+          "02 Jan",
+          "03 Jan",
+          "04 Jan",
+          "05 Jan",
+          "06 Jan",
+          "07 Jan",
+          "08 Jan",
+          "09 Jan",
+          "10 Jan",
+          "11 Jan",
+          "12 Jan",
         ],
       },
       tooltip: {
@@ -59,29 +74,29 @@ export default function DashedChart() {
           {
             title: {
               formatter: function (val) {
-                return val + " (mins)"
-              }
-            }
+                return val + " (mins)";
+              },
+            },
           },
           {
             title: {
               formatter: function (val) {
-                return val + " per session"
-              }
-            }
+                return val + " per session";
+              },
+            },
           },
           {
             title: {
               formatter: function (val) {
                 return val;
-              }
-            }
-          }
-        ]
+              },
+            },
+          },
+        ],
       },
       grid: {
-        borderColor: '#f1f1f1',
-      }
+        borderColor: "#f1f1f1",
+      },
     },
   };
   return (
--- frontend/src/components/GraphModal.js
+++ frontend/src/components/GraphModal.js
@@ -73,7 +73,7 @@ export default (props) => {
               </div>
             </div>
 
-            <DashedChart />
+            {/* <DashedChart /> */}
           </div>
         </div>
       </div>
